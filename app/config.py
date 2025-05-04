3357cefd4c43fbb58e0addc6a7cbb93810c9861b
Urszula <ulach@hotmail.co.uk>
2025-05-04 09:50:29 +0100

    Credentials changed

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 # Database configuration
-HOST = "localhost"
-USER = "ula"
-PASSWORD = "Gordito10?"
-DATABASE = "patient_survey_db"
-PORT = '3306'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load from .env file
+
+HOST = os.getenv('HOST', 'localhost')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+DATABASE = os.getenv('DATABASE', 'patient_survey_db')
