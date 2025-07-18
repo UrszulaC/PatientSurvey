a0cd61c664251f2fb7015681faf660683db09ac2
UrszulaC <63776006+UrszulaC@users.noreply.github.com>
2025-07-18 12:14:26 +0100

    Variables added from the Jenkins

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "db_user" {
+  description = "Database username for the survey app"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password for the survey app"
+  type        = string
+  sensitive   = true
+}
 
