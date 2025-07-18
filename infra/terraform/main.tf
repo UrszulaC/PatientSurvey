eda4da0d2e46b28e723fe5a2bdfd2b9a3b46a77e
UrszulaC <63776006+UrszulaC@users.noreply.github.com>
2025-07-18 17:05:47 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.36"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -11,50 +15,57 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_resource_group" "existing" {
+  name = "MyPatientSurveyRG"
+}
+
 resource "random_integer" "suffix" {
   min = 1000
   max = 9999
 }
 
-data "azurerm_resource_group" "existing" {
-  name = "MyPatientSurveyRG"
-}
-
 resource "azurerm_container_group" "survey_app" {
   name                = "survey-app-cg"
-  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
   os_type             = "Linux"
   restart_policy      = "OnFailure"
 
+  # public IP + DNS
   ip_address_type = "Public"
   dns_name_label  = "survey-app-${random_integer.suffix.result}"
 
+  # allow inbound on 8000
   ports {
     port     = 8000
     protocol = "TCP"
   }
-  
 
   container {
     name  = "survey-app"
     image = "urszulach/epa-feedback-app:latest"
+    cpu    = "0.5"       # top-level in container
+    memory = "1.0"       # top-level in container
+
+    # container-specific port
     ports {
       port     = 8000
       protocol = "TCP"
     }
-  environment_variables = {
+
+    environment_variables = {
       DB_HOST     = "172.17.0.1"
       DB_NAME     = "patient_survey_db"
       DB_USER     = var.db_user
       DB_PASSWORD = var.db_password
     }
-    resources {
-      cpu    = 0.5
-      memory = "1.0"
-    } 
   }
 }
+
 output "survey_app_fqdn" {
   value = azurerm_container_group.survey_app.fqdn
+}
+
+output "survey_app_public_ip" {
+  value = azurerm_container_group.survey_app.ip_address
 }
