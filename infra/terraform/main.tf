727887aeba7b1711050d05e888eeba36db21cd19
UrszulaC <63776006+UrszulaC@users.noreply.github.com>
2025-09-09 20:17:07 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,25 +4,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.36"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "f8710f06-734a-4570-941f-8a779d917b29"
 }
 
 data "azurerm_resource_group" "existing" {
   name = "MyPatientSurveyRG"
-}
-
-resource "random_integer" "suffix" {
-  min = 1000
-  max = 9999
 }
 
 resource "azurerm_container_group" "survey_app" {
@@ -30,11 +20,10 @@ resource "azurerm_container_group" "survey_app" {
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
   os_type             = "Linux"
-  restart_policy      = "OnFailure"
+  restart_policy      = "Always"
 
-  # public IP + DNS
   ip_address_type = "Public"
-  dns_name_label  = "survey-app-${random_integer.suffix.result}"
+  dns_name_label  = "survey-app"
 
   container {
     name  = "survey-app"
@@ -42,9 +31,8 @@ resource "azurerm_container_group" "survey_app" {
     cpu   = "0.5"
     memory = "1.0"
 
-    # container-specific port
     ports {
-      port     = 8000
+      port     = 8001   # App metrics port
       protocol = "TCP"
     }
 
@@ -55,11 +43,16 @@ resource "azurerm_container_group" "survey_app" {
       DB_PASSWORD = var.db_password
     }
   }
+
+  tags = {
+    purpose = "EPA Patient Survey App"
+  }
 }
 
 output "survey_app_fqdn" {
   value = azurerm_container_group.survey_app.fqdn
 }
+
 output "survey_app_public_ip" {
   value = azurerm_container_group.survey_app.ip_address
 }
