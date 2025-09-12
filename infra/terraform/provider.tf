3ef8337fa53268a4178e512ca95cabe36f29822a
UrszulaC <63776006+UrszulaC@users.noreply.github.com>
2025-09-12 12:57:22 +0100

    Update provider.tf

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,10 +1,1 @@
-# provider.tf
-provider "azurerm" {
-  features {}
-  
-  # Use Azure CLI authentication instead of service principal
-  use_cli = true
-  
-  # Optional: If you know your subscription ID, you can set it here
-  subscription_id = "f8710f06-734a-4570-941f-8a779d917b29"
-}
+
