15883be1715c5e60bd6b16b25d4f6cc9701ef1ca
UrszulaC <63776006+UrszulaC@users.noreply.github.com>
2025-09-18 21:12:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Initialize Flask app
-app = Flask(__name__, template_folder='/app/templates')
+app = Flask(__name__)
 
 # Prometheus metrics
 def get_or_create_counter(name, description, registry=None):
